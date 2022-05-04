21e9c2c654deebaecd9d6cd92fea1bb65de53c7c
Ly Kien Minh <lykienminh9@gmail.com>
2022-05-05 00:36:58 +0700

    handle cookies and random url thread

--- testComment.py
+++ testComment.py
@@ -1,3 +1,4 @@
+from imaplib import _Authenticator
 import unittest
 
 from parameterized import parameterized
@@ -10,7 +11,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+import pickle
+import random
 from ddt import ddt, data, unpack
 
 class readDatatest:
@@ -34,14 +38,32 @@ class readDatatest:
     return values
 
 dataTests = readDatatest().dataTestComment()
+isLogin = False
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# executor_url = driver.command_executor._url
+# session_id = driver.session_id
 
 # @ddt
 class MyTestCase(unittest.TestCase):
   def setUp(self):
-    print("========== [ Begin Test ] ==========")
-    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print("\n========== [ Begin Test ] ==========")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     self.driver.get('https://tinhte.vn/')
-    stepComment(self.driver).login("bugbugphapha@gmail.com", "bugpha")
+
+    global isLogin
+    if not isLogin:
+      stepComment(self.driver).login("bugbugphapha@gmail.com", "bugpha", "bugpha")
+      pickle.dump(self.driver.get_cookies() , open("cookies.pkl","wb"))
+      isLogin = True
+    else:
+      cookies = pickle.load(open("cookies.pkl", "rb"))
+      for cookie in cookies:
+        self.driver.add_cookie(cookie)
+
 
   def tearDown(self):
     self.driver.quit()
@@ -50,24 +72,31 @@ class MyTestCase(unittest.TestCase):
 
   @parameterized.expand(dataTests)
   # @unpack
-  def test_comment(self, no, url, comment, desiredResult, desiredMessage):
-    stepComment(self.driver).comment("https://tinhte.vn/thread/nhung-soi-cap-usb4-240w-dau-tien-da-xuat-hien-con-cho-thiet-bi-va-bo-sac-nua-thoi.3513195/", comment)
-    # self.assertIn(desiredMessage, verifyLogin(self.browser).login())  
+  def test_comment(self, no, comment, desiredResult, desiredMessage):
+    stepComment(self.driver).comment(comment)
+    # self.assertIn(desiredMessage, verifyLogin(self.browser).login())
 
 class stepComment:
   def __init__(self, driver):
     self.driver = driver
+    self.timeout = 60
+    self.author = 'Lee Kim Min'
 
-  def login(self, username, password):
+  # @classmethod
+  def updateAuthor(cls, value):
+      cls.author = value
+
+  def login(self, username, password, authorName):
+    # self.author = authorName
+    self.updateAuthor(authorName)
     print("[Step] Login")
 
-    self.timeout = 60
     self.driver.find_element(By.CSS_SELECTOR, 'button').click()
 
     link_SignIn = self.driver.find_element(By.CSS_SELECTOR, "a[href='https://tinhte.vn/login/']")
     link_SignIn.click()
     
-    WebDriverWait(self.driver,self.timeout).until(
+    WebDriverWait(self.driver, self.timeout).until(
       EC.presence_of_element_located((By.CSS_SELECTOR, '#ctrl_pageLogin_login2'))
     )
 
@@ -83,24 +112,42 @@ class stepComment:
       EC.presence_of_element_located((By.CSS_SELECTOR, "div.main-page"))
     )
   
-  def comment(self, url, input_value, expected_result=""):
-    print("[Step] Comment")
-    self.driver.get(url)
+  def comment(self, input_value):
+    print("[Step] Comment by " + self.author)
+    self.driver.get(self.randomUrl())
 
-    WebDriverWait(self.driver,self.timeout).until(
-      EC.presence_of_element_located((By.CSS_SELECTOR, "textarea"))
+    WebDriverWait(self.driver, self.timeout).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, "div.thread-view--content-wrapper"))
     )
 
-    txt_cmt=self.driver.find_element(By.CSS_SELECTOR, "textarea")
+    time.sleep(9)
+
+    txt_cmt=self.driver.find_element(By.XPATH, "//textarea[@class='post-reply-input']")
+    self.driver.execute_script("arguments[0].scrollIntoView();", txt_cmt)
     txt_cmt.send_keys(input_value)
 
-    btn_reply = self.driver.find_element(By.CLASS_NAME, 'post-reply-submit')
+    # actions = ActionChains(self.driver)
+    # actions.move_to_element(txt_cmt).perform()
+
+    btn_reply = self.driver.find_element(By.XPATH, "//button[contains(@class, 'post-reply-submit')]")
     btn_reply.click()
 
-    time.sleep(20)
-    # cmt=self.driver.find_element(By.CSS_SELECTOR,"article[data-author='minhhoang0411']:last-child article .bbWrapper")
+    time.sleep(14)
 
-    # self.assertEqual(expected_result, cmt.text)
+    cmt = self.driver.find_element(By.CSS_SELECTOR, "div[data-author='" + self.author + "'] span")
+
+    assert input_value == cmt.text
+    time.sleep(14)
+    
+  def randomUrl(self):
+    print("[Step] Random thread")
+    threadComponent = self.driver.find_elements(By.XPATH, "//div[contains(@class, 'thread-containers')]/ol/li/div/article/*[1][name()='a']")
+    
+    # links = [thread.get_attribute('href') for thread in threadComponent]
+    selectedThread = random.choice(threadComponent)
+    self.driver.execute_script("arguments[0].scrollIntoView();", selectedThread)
+
+    return selectedThread.get_attribute('href')
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
